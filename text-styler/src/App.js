c566ff41a77996b114d97fa4a07664d857ab11f9
omg2804 <gaikwadom992@gmail.com>
2025-03-26 01:51:23 +0530

    final

--- text-styler/src/App.js
+++ text-styler/src/App.js
@@ -1,19 +1,54 @@
-import React from "react";
-import { MantineProvider, Container, Title } from "@mantine/core";
-import TextStyler from "./components/TextStyler"; // Corrected path
+import React, { useRef } from "react";
+import { Button } from "@mantine/core";
 
+function TextStyler() {
+  const editorRef = useRef(null);
 
-function App() {
+  const applyStyle = (command, value = null) => {
+    document.execCommand(command, false, value);
+  };
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
-      <Container>
-        <Title order={1} align="center" mt="md">
-          Create Your Text
-        </Title>
-        <TextStyler />
-      </Container>
-    </MantineProvider>
+    <div style={{ padding: "20px", fontFamily: "Arial" }}>
+      <h2>Text Styler</h2>
+
+      {/* Foreground Color Picker */}
+      <label>Foreground: </label>
+      <input
+        type="color"
+        onChange={(e) => applyStyle("foreColor", e.target.value)}
+        style={{ marginRight: "10px" }}
+      />
+
+      {/* Background Color Picker */}
+      <label>Background: </label>
+      <input
+        type="color"
+        onChange={(e) => applyStyle("backColor", e.target.value)}
+      />
+
+      <div style={{ marginTop: "10px" }}>
+        <Button onClick={() => applyStyle("bold")} style={{ marginRight: "5px" }}>
+          Bold
+        </Button>
+        <Button onClick={() => applyStyle("underline")}>Underline</Button>
+      </div>
+
+      {/* Editable Text Area */}
+      <div
+        ref={editorRef}
+        contentEditable
+        style={{
+          border: "1px solid black",
+          minHeight: "100px",
+          marginTop: "10px",
+          padding: "10px",
+        }}
+      >
+        Select text and apply styles...
+      </div>
+    </div>
   );
 }
 
-export default App;
+export default TextStyler;
--- text-styler/src/index.js
+++ text-styler/src/index.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MantineProvider } from "@mantine/core";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
